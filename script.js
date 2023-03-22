74a9c4826912077c0b2c83dcd8a08133ffa3a768
melvin <melvintk09@gmail.com>
2023-03-22 09:25:28 +0530

    function to play a single round of game is added.

--- script.js
+++ script.js
@@ -6,4 +6,28 @@ function getComputerChoice(){
 }
 
 const choice = getComputerChoice();
-console.log(choice);
+//console.log(choice);
+
+let playerSelection = prompt("enter your choice",);
+let computerSelection = getComputerChoice();
+
+function oneRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    
+    if (playerSelection === computerSelection){
+        return"it's a tie";
+    }
+    else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+      ) {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+      } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+      }
+
+}
+
+const result = oneRound(playerSelection,computerSelection);
+console.log(result);
