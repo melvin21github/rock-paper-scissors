934708854fcb89c778ca69491124e4c912b150ee
melvin <melvintk09@gmail.com>
2023-05-30 20:41:39 +0530

    buttons were added. game processed using dom methods..

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function getComputerChoice() {
     const choices = ["rock", "scissors", "paper"];
     const randomIndex = Math.floor(Math.random() * choices.length);
-    /*console.log(randomIndex);*/
+    //console.log(randomIndex);
     return choices[randomIndex];
 }
 
-const choice = getComputerChoice();
+//const choice = getComputerChoice();
 //console.log(choice);
 
 //let playerSelection = prompt("enter your choice",);
-let computerSelection = getComputerChoice();
+//let computerSelection = getComputerChoice();
 
 function oneRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-
+    //playerSelection = playerSelection.toLowerCase();
+    //console.log(playerSelection);
     if (playerSelection === computerSelection) {
         return "it's a tie";
     }
@@ -33,7 +33,7 @@ function oneRound(playerSelection, computerSelection) {
 //console.log(result);
 
 
-function game() {
+/*function game() {
     console.log("this is a 5 round game");
     for (i = 0; i < 5; i++) {
         console.log(`round ${i}`);
@@ -45,4 +45,53 @@ function game() {
     }
 }
 
-game();
+game();*/
+
+//adding buttons 
+
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+//console.log(rock);
+
+rock.addEventListener('click', () =>{
+    let playerSelection = 'rock';
+    //console.log(playerSelection);
+    playerChoice.textContent='you chose :rock';
+    let computerSelection = getComputerChoice();
+    compChoice.textContent=`the computer chose :${computerSelection}`;
+    const result1 = oneRound(playerSelection,computerSelection);
+    //console.log(result1);
+    result.textContent = `the outcome of the round:${result1}`;
+});
+
+paper.addEventListener('click', () =>{
+    let playerSelection = 'paper';
+    //console.log(playerSelection);
+    playerChoice.textContent='you chose: paper';
+    let computerSelection = getComputerChoice();
+    compChoice.textContent=`the computer chose :${computerSelection}`;
+    const result1 = oneRound(playerSelection,computerSelection);
+    //console.log(result1);
+    result.textContent = `the outcome of the round:${result1}`;
+});
+
+scissors.addEventListener('click', () =>{
+    let playerSelection = 'scissors';
+    //console.log(playerSelection);
+    playerChoice.textContent='you chose :scissors';
+    let computerSelection = getComputerChoice();
+    compChoice.textContent=`the computer chose :${computerSelection}`;
+    const result1 = oneRound(playerSelection,computerSelection);
+    //console.log(result1);
+    result.textContent = `the outcome of the round:${result1}`;
+});
+
+const result = document.createElement('div');
+result.classList.add('result');
+//result.textContent = `the outcome of the round:`;
+const outcome = document.querySelector('#outcome');
+outcome.appendChild(result);
+
+const playerChoice= document.getElementById('playerChoice');
+const compChoice= document.getElementById('compChoice');
